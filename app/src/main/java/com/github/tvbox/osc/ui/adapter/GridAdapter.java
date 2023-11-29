306c0d40e0db328adf1bde759c0c8dd1140eaa51
laoZ <71415980+xyzjhe@users.noreply.github.com>
2023-11-29 15:23:35 +0800

    Update GridAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/GridAdapter.java
@@ -12,6 +12,7 @@ import com.github.tvbox.osc.bean.Movie;
 import com.github.tvbox.osc.picasso.RoundTransformation;
 import com.github.tvbox.osc.util.DefaultConfig;
 import com.github.tvbox.osc.util.MD5;
+import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -24,14 +25,37 @@ import me.jessyan.autosize.utils.AutoSizeUtils;
  * @description:
  */
 public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
+    private boolean mShowList = false;
 
-    public GridAdapter() {
-        super(R.layout.item_grid, new ArrayList<>());
-
+    public GridAdapter(boolean l) {
+        super( l ? R.layout.item_list:R.layout.item_grid, new ArrayList<>());
+        this.mShowList = l;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, Movie.Video item) {
+        if(this.mShowList) {
+            helper.setText(R.id.tvNote, item.note);
+            helper.setText(R.id.tvName, item.name);
+            ImageView ivThumb = helper.getView(R.id.ivThumb);
+            //由于部分电视机使用glide报错
+            if (!TextUtils.isEmpty(item.pic)) {
+                item.pic=item.pic.trim();
+                Picasso.get()
+                        .load(DefaultConfig.checkReplaceProxy(item.pic))
+                        .transform(new RoundTransformation(MD5.string2MD5(item.pic))
+                                .centerCorp(true)
+                                .override(AutoSizeUtils.mm2px(mContext, 240), AutoSizeUtils.mm2px(mContext, 320))
+                                .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
+                        .placeholder(R.drawable.img_loading_placeholder)
+                        .noFade()
+//                        .error(R.drawable.img_loading_placeholder)
+                        .into(ivThumb);
+            } else {
+                ivThumb.setImageResource(R.drawable.img_loading_placeholder);
+            }
+            return;
+        }
 
         TextView tvYear = helper.getView(R.id.tvYear);
         if (item.year <= 0) {
@@ -40,7 +64,22 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
             tvYear.setText(String.valueOf(item.year));
             tvYear.setVisibility(View.VISIBLE);
         }
-
+        TextView tvLang = helper.getView(R.id.tvLang);
+        tvLang.setVisibility(View.GONE);
+        /*if (TextUtils.isEmpty(item.lang)) {
+            tvLang.setVisibility(View.GONE);
+        } else {
+            tvLang.setText(item.lang);
+            tvLang.setVisibility(View.VISIBLE);
+        }*/
+        TextView tvArea = helper.getView(R.id.tvArea);
+        tvArea.setVisibility(View.GONE);
+        /*if (TextUtils.isEmpty(item.area)) {
+            tvArea.setVisibility(View.GONE);
+        } else {
+            tvArea.setText(item.area);
+            tvArea.setVisibility(View.VISIBLE);
+        }*/
         if (TextUtils.isEmpty(item.note)) {
             helper.setVisible(R.id.tvNote, false);
         } else {
@@ -48,13 +87,20 @@ public class GridAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHolder> {
             helper.setText(R.id.tvNote, item.note);
         }
         helper.setText(R.id.tvName, item.name);
+        helper.setText(R.id.tvActor, item.actor);
         ImageView ivThumb = helper.getView(R.id.ivThumb);
         //由于部分电视机使用glide报错
         if (!TextUtils.isEmpty(item.pic)) {
+            item.pic=item.pic.trim();
             Picasso.get()
                     .load(DefaultConfig.checkReplaceProxy(item.pic))
+                    .transform(new RoundTransformation(MD5.string2MD5(item.pic))
+                            .centerCorp(true)
+                            .override(AutoSizeUtils.mm2px(mContext, 240), AutoSizeUtils.mm2px(mContext, 320))
+                            .roundRadius(AutoSizeUtils.mm2px(mContext, 10), RoundTransformation.RoundType.ALL))
                     .placeholder(R.drawable.img_loading_placeholder)
-                    .error(R.drawable.img_loading_placeholder)
+                    .noFade()
+//                    .error(R.drawable.img_loading_placeholder)
                     .into(ivThumb);
         } else {
             ivThumb.setImageResource(R.drawable.img_loading_placeholder);
